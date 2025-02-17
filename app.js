cb07de41d3e0f7b946a0d32096492bf001b5fda3
RomanOlsen <romanjolsen@gmail.com>
2025-02-17 16:07:12 -0700

    byu scoring is more clean than bsu's rn

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 let bsu = 0
 let byu = 0
+let byuUpdate = 0
 
-function updateBSU(bsu) {
+function bsuScored1() {
+  bsu = bsu + 1
   console.log(bsu)
   document.getElementById("bsuBoard")
   bsuBoard.innerText = bsu
 }
 
-function updateBYU() {
-  console.log(byu)
+function bsuScored2() {
+  bsu = bsu + 2
+  console.log(bsu)
   document.getElementById("bsuBoard")
+  bsuBoard.innerText = bsu
+}
+
+function bsuScored3() {
+  bsu = bsu + 3
+  console.log(bsu)
+  document.getElementById("bsuBoard")
+  bsuBoard.innerText = bsu
+}
+
+function bsuLost1() {
+  bsu = bsu - 1
+  console.log(bsu)
+  document.getElementById("bsuBoard")
+  bsuBoard.innerText = bsu
+}
+
+function updateBYU(byuUpdate) {
+  byu = byu + byuUpdate
+  console.log(byu)
+  document.getElementById("byuBoard")
+  byuBoard.innerText = byu
 }
 
 function reset() {
