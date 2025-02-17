8998c09986a62b840696f1f67a0789b2087a0bb9
RomanOlsen <romanjolsen@gmail.com>
2025-02-17 16:39:17 -0700

    Learning the if statements - now cant go below 0

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ let byu = 0
 let updateNumber = 0 // The current number being clicked on
 
 function updateBSU(updateNumber) {
-  bsu = bsu + updateNumber
-  console.log(bsu)
-  document.getElementById("bsuBoard")
-  bsuBoard.innerText = bsu
+  if (bsu + updateNumber < 0) {
+    console.log("You cant do that!")
+  }
+  else {
+    bsu = bsu + updateNumber
+    console.log(bsu)
+    document.getElementById("bsuBoard")
+    bsuBoard.innerText = bsu
+  }
 }
 
 function updateBYU(updateNumber) {
-  byu = byu + updateNumber
-  console.log(byu)
-  document.getElementById("byuBoard")
-  byuBoard.innerText = byu
+
+  if (byu + updateNumber < 0) {
+    console.log("You cant do that!")
+  }
+  else {
+    byu = byu + updateNumber
+    console.log(byu)
+    document.getElementById("byuBoard")
+    byuBoard.innerText = byu
+  }
 }
 
 function reset() {
