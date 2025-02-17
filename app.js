a3a4476185245966d7e2cbb12dfc6226c92b4519
RomanOlsen <romanjolsen@gmail.com>
2025-02-17 16:56:36 -0700

    Won message added

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ function updateBSU(updateNumber) {
   if (bsu + updateNumber < 0) {
     console.log("You cant do that!")
   }
-  else {
+  else if (bsu + updateNumber > 100) {
+    console.log("BSU reached 100. GG")
+    bsuWonBoard.classList.remove('d-none')
+  } else {
     bsu = bsu + updateNumber
     console.log(bsu)
     document.getElementById("bsuBoard")
@@ -14,12 +17,18 @@ function updateBSU(updateNumber) {
   }
 }
 
+//   capybaraElem.classList.remove('d-none') from Jeremy's lecture.
+
 function updateBYU(updateNumber) {
 
   if (byu + updateNumber < 0) {
     console.log("You cant do that!")
   }
-  else {
+  else if (byu + updateNumber > 100) {
+    console.log("BYU reached 100. GG")
+    document.getElementById("byuWonBoard")
+    byuWonBoard.classList.remove('d-none')
+  } else {
     byu = byu + updateNumber
     console.log(byu)
     document.getElementById("byuBoard")
@@ -32,5 +41,7 @@ function reset() {
   byu = 0
   bsuBoard.innerText = bsu
   byuBoard.innerText = byu
+  bsuWonBoard.classList.add('d-none')
+  byuWonBoard.classList.add('d-none')
 }
 
