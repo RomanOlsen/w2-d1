5133fd488e9875e9d19a1311a17c63926a26de29
RomanOlsen <romanjolsen@gmail.com>
2025-02-17 15:40:56 -0700

    bsu vs byu start

---
+++ app.js
@@ -0,0 +1,15 @@
+let bsu = 0
+let byu = 0
+
+function updateBSU(bsu) {
+  console.log(bsu)
+  bsuBoard.innerText = bsu
+  document.getElementById("bsuBoard")
+}
+
+function updateBYU() {
+  console.log(byu)
+  document.getElementById("bsuBoard")
+}
+
+// TODO find a way to make updateBSU and BYU into 1 function
